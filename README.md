03aa30f8566053c20038e70b1e4b400a728efccb
tdolan21 <timmydolan21@gmail.com>
2023-12-13 13:08:27 -0500

    added session state and export functions

--- README.md
+++ README.md
@@ -1,20 +1,41 @@
-# Vega RealTime ImageGen
+# Segmind Vega Real time TTI in Streamlit
 
 Realtime image generation using segmind vega RT latent consistency adapter and streamlit + streamlit-keyup
 
-[demo_video.webm](https://github.com/tdolan21/vega-rt-streamlit/assets/40906019/79994ceb-b859-4e8f-8dfc-26392466fcfe)
+![header](assets/header.png)
+
+![Python 3.10](https://img.shields.io/badge/python-3.10-blue.svg)
+![Streamlit](https://img.shields.io/badge/Streamlit-FF4B4B.svg?&logo=Streamlit&logoColor=purple)
+![Hugging Face](https://img.shields.io/badge/Hugging%20Face-F7931E.svg?&logo=HuggingFace&logoColor=cyan)
+
+
+## Demo Video
 
 This demo assumes you have torch and CUDA properly installed for your machine.
 
+[demo_video.webm](https://github.com/tdolan21/vega-rt-streamlit/assets/40906019/79994ceb-b859-4e8f-8dfc-26392466fcfe)
+
 Demo uses just under 16GB of VRAM
 
-## Prerequisites
 
-```
-pip install streamlit streamlit-keyup transformers diffusers accelerate
-```
+## Features
+
+### Downloadable Images
+
++ **Instant Download**: Once you're satisfied with the generated image, you can effortlessly download it. This feature ensures that you can save your creations directly from the session state, preserving the quality and details of the image.
+
+### Efficient Storage Strategy
+
++ **Selective Storage**: To optimize storage usage, not every image is stored permanently. However, each generated image is meticulously tracked.
++ **Session State Tracking**: Every image is assigned a unique identifier (UUID) and timestamped when stored in the session state. This approach facilitates easy tracking and organization of your images.
+
+### Export Option
+
++ **Session Analysis**: For those interested in analyzing the progression of their prompts, the application provides an option to export the entire session state. This feature allows for a comprehensive review of your creative journey.
 
 ## Installation
+
+Follow these simple steps to set up the project on your local machine:
 
 ```bash
 git clone https://github.com/tdolan21/vega-rt-streamlit
@@ -22,11 +43,19 @@ cd vega-rt-streamlit
 pip install -r requirements.txt
 ```
 
+This will ensure that all necessary libraries and dependencies are correctly installed, setting up the environment for smooth operation.
+
 ## Usage
+
+To use the application, simply run the following command in your terminal:
+
 
 ```
 streamlit run app.py
 ```
+
+This command will start the Streamlit server and open the application in your default web browser. Enjoy exploring the features and functionalities of the application with ease.
+
 ## More information 
 
 [Model-Card](https://huggingface.co/segmind/Segmind-VegaRT)
--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-import streamlit as st
-import torch
 from diffusers import LCMScheduler, AutoPipelineForText2Image
-from PIL import Image
-import io
 from st_keyup import st_keyup
+import streamlit as st
+from PIL import Image
+import datetime
+import torch
+import uuid
+import io
+import os
+
+st.set_page_config(
+    page_title="Real-Time Text to Image Generation",
+    page_icon="🎨",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
 
 @st.cache_resource
 def load_model():
@@ -22,36 +32,93 @@ def generate_image(pipe, prompt, num_inference_steps, guidance_scale):
     image = pipe(prompt=prompt, num_inference_steps=num_inference_steps, guidance_scale=guidance_scale).images[0]
     return image
 
+def save_image(image_buffer, filename):
+    # Save the image
+    try:
+        with open(filename, 'wb') as f:
+            f.write(image_buffer.getbuffer())
+        return filename
+    except IOError as e:
+        return f"Error saving file: {e}"
+
+def export_all_images(image_history):
+    # Create a unique subfolder in the outputs directory
+    export_folder = os.path.join('outputs', uuid.uuid4().hex)
+    if not os.path.exists(export_folder):
+        os.makedirs(export_folder)
+
+    saved_files = []
+    for item in image_history:
+        file_path = os.path.join(export_folder, item['filename'])
+        result = save_image(item['image'], file_path)
+        if "Error" not in result:
+            saved_files.append(result)
+
+    return saved_files, export_folder
+
 def main():
-    st.title("Real-Time Text to Image Generation with Keyup")
+
+    st.title("Segmind Vega-RT")
+    st.sidebar.image("assets/header.png")
+    st.sidebar.title("Parameters")
     
     debounce = st.sidebar.slider("Debounce", 0, 1000, 500)
     prompt = st_keyup("Enter your prompt:", debounce=debounce, key="prompt_input")
 
-    # Parameters for image generation in the sidebar
     num_inference_steps = st.sidebar.slider("Number of Inference Steps", 2, 8, 4)
     guidance_scale = st.sidebar.slider("Guidance Scale", 0.0, 2.0, 0.0)
 
-    # Initialize an empty buffer for the image
-    img_buffer = None
+    if 'image_history' not in st.session_state:
+        st.session_state.image_history = []
 
-    # Displaying the image
-    if prompt:  # Check if prompt is not empty
-        with st.spinner("Generating Image..."):
-            image = generate_image(pipe, prompt, num_inference_steps, guidance_scale)
-            img_buffer = io.BytesIO()
-            image.save(img_buffer, format="JPEG")
-            img_buffer.seek(0)
-            st.image(img_buffer, use_column_width=True)
+    # Generate image only if the prompt changes
+    if prompt:
+        if 'last_prompt' not in st.session_state or prompt != st.session_state.last_prompt:
+            with st.spinner("Generating Image..."):
+                image = generate_image(pipe, prompt, num_inference_steps, guidance_scale)
+                img_buffer = io.BytesIO()
+                image.save(img_buffer, format="JPEG")
+                img_buffer.seek(0)
 
-    # Save Image button in the sidebar
-    if img_buffer is not None:
-        st.sidebar.download_button(
-            label="Save Current Image",
-            data=img_buffer,
-            file_name="outputs/generated_image.jpeg",
-            mime="image/jpeg"
-        )
+               
+                timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+                unique_id = uuid.uuid4().hex
+                filename = f"generated_image_{timestamp}_{unique_id}.jpeg"
+
+                st.session_state.image_history.insert(0, {'prompt': prompt, 'image': img_buffer, 'filename': filename})
+
+                st.session_state.image_history = st.session_state.image_history[:10]
+
+                st.session_state.last_prompt = prompt
+
+    # Display and save the current image
+    if st.session_state.image_history:
+        current_image = st.session_state.image_history[0]['image']
+        current_image.seek(0)  # Reset the buffer to the start
+        st.image(current_image, use_column_width=True)
+
+        st.sidebar.divider()
+
+        # Save to 'outputs' folder and provide download link
+        if st.sidebar.button('Save Current Image to Folder'):
+            filename = st.session_state.image_history[0]['filename']
+            save_result = save_image(current_image, os.path.join('outputs', filename))
+            if "Error" in save_result:
+                st.sidebar.error(save_result)
+            else:
+                st.sidebar.success(f"Image saved as {save_result}")
+
+        # Button to export all images
+        if st.sidebar.button('Export All Images'):
+            saved_files, export_folder = export_all_images(st.session_state.image_history)
+            if saved_files:
+                st.sidebar.success(f"All images exported to {export_folder}")
+            else:
+                st.sidebar.error("Error exporting images")
+        
+        st.sidebar.divider()
 
 if __name__ == "__main__":
     main()
+
+
